942350226996c44714e1d4d7db6fc2cb91e126e7
Michael Oceguera <syntaxmike91@gmail.com>
2023-06-15 16:11:52 -0700

    Add user input to adjust size of grid

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,15 +1,34 @@
 const container = document.querySelector('.container');
 
-function createGrid() {
-    for(let rows = 0; rows < 16; rows++) {
-        for(let columns = 0; columns < 16; columns++){
+function createGrid(gridNum = 16) {
+    for(let rows = 0; rows < gridNum; rows++) {
+        for(let columns = 0; columns < gridNum; columns++){
             const cell = document.createElement('div');
             cell.className = 'grid';
-            cell.style.width = `${720/16}px`;
-            cell.style.height = `${720/16}px`;
+            cell.style.width = `${720/gridNum}px`;
+            cell.style.height = `${720/gridNum}px`;
             container.appendChild(cell);
         }
     }
 }
+
+function removeCurrentGrid() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+const btnSubmit = document.querySelector('button');
+
+btnSubmit.addEventListener('click', function(e) {
+    const userInput = +document.getElementById('gridNum').value;
+
+    if(isNaN(userInput) || userInput > 100 || userInput < 0) {
+        alert(`Please enter a number between 1 - 100`)
+    } else {
+        removeCurrentGrid();
+        createGrid(userInput);
+    }
+})
 
 createGrid();
