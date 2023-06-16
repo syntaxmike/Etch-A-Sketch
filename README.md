90f25a605309f3c2a397bb9345cd844ec35edb59
Michael Oceguera <syntaxmike91@gmail.com>
2023-06-16 14:51:50 -0700

    Add options for different color output

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Etch-A-Sketch
 
+A 16x16 (by default) grid of squares that will change color when hovered over.
+
 <a href="https://syntaxmike.github.io/Etch-A-Sketch/" rel="nofollow">Live Demo</a>
--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,13 +1,30 @@
-const container = document.querySelector('.container');
-const btnSubmit = document.querySelector('button');
+const DEFAULT_COLOR = 'black';
+let currentColor = DEFAULT_COLOR;
+let mouseDown = false;
 
-function createGrid(gridNum = 16) {
-    for(let rows = 0; rows < gridNum; rows++) {
-        for(let columns = 0; columns < gridNum; columns++){
+const container = document.querySelector('.container');
+const btnSubmit = document.querySelector('#submit');
+const btnBlackWhite = document.querySelector('#black');
+const btnRainbow = document.querySelector('#rainbow');
+const btnShader = document.querySelector('#shader');
+
+
+btnSubmit.addEventListener('click', getUserInput);
+btnRainbow.addEventListener('click', setCurrentColor);
+btnShader.addEventListener('click', setCurrentColor);
+btnBlackWhite.addEventListener('click', setCurrentColor)
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function createGrid(gridCount = 16) {
+    for(let rows = 0; rows < gridCount; rows++) {
+        for(let columns = 0; columns < gridCount; columns++){
             const cell = document.createElement('div');
             cell.className = 'grid';
-            cell.style.width = `${720/gridNum}px`;
-            cell.style.height = `${720/gridNum}px`;
+            cell.style.width = `${container.clientHeight / gridCount}px`;
+            cell.style.height = `${container.clientHeight / gridCount}px`;
+            cell.addEventListener('mouseover', changeColor);
+            cell.addEventListener('mousedown', changeColor);
             container.appendChild(cell);
         }
     }
@@ -15,17 +32,17 @@ function createGrid(gridNum = 16) {
 
 function changeColor(e) {
     let targetCell = e.target;
-    if(targetCell.className === 'grid') {
+    if (e.type === 'mouseover' && !mouseDown) return;
+    if(currentColor === 'black'){
+        targetCell.style.backgroundColor = '#000000';
+    } else if (currentColor === 'rainbow') {
         targetCell.style.backgroundColor =`#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    } else if (currentColor === 'shader') {
+        targetCell.style.backgroundColor = `hsl(${Math.floor(Math.random() * 100)}, ${Math.floor(Math.random() * 100)}%, ${Math.floor(Math.random() * 50)}% )`
     }
 
 }
 
-function removeCurrentGrid() {
-    while(container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-}
 
 function getUserInput() {
     const userInput = +document.getElementById('gridNum').value;
@@ -38,10 +55,27 @@ function getUserInput() {
     }
 }
 
+function setCurrentColor(e) {
+    currentColor = e.srcElement.id;
+}
 
-btnSubmit.addEventListener('click', getUserInput);
-window.addEventListener('mouseover', changeColor);
+function resize() {
+    const newHeight =  container.clientHeight / Math.sqrt(container.childElementCount);
+    const newWidth =  container.clientWidth / Math.sqrt(container.childElementCount);
+    Array.from(container.children).forEach((cell) => {
+        cell.style.width = `${newWidth}px`;
+        cell.style.height = `${newHeight}px`;
+    })
+}
+
+function removeCurrentGrid() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
 window.onload = () => {
     createGrid();
 }
+
+window.addEventListener('resize', resize)
